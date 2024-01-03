18ea1724a29975b84909d300d667468dba06f015
татьяна090975 <tatyana090975@gmail.com>
2024-01-04 03:15:16 +0400

    Задание 4.3.14

--- Program.cs
+++ Program.cs
@@ -10,16 +10,19 @@ namespace Mod4BasicOfC
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int[][] array = new int[3][];
 
-            int sum = 0;
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-            foreach (var item in arr)
+            foreach (var num in array)
             {
-                sum = sum + item;               
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
             }
-
-            Console.WriteLine(sum);
             Console.ReadKey();
          
         }
