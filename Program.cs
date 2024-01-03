f63933fed10f472643bb710f1926ebb234ef41d0
татьяна090975 <tatyana090975@gmail.com>
2024-01-04 00:49:14 +0400

    Массив

--- Program.cs
+++ Program.cs
@@ -10,30 +10,49 @@ namespace Mod4BasicOfC
     {
         static void Main(string[] args)
         {
-            int sum = 0;
+            string[] favcolors = new string[3];
 
-            while (true)
+            for (int i = 0; i < favcolors.Length; i++)
             {
-                Console.WriteLine("Введите число");
-                var number = Convert.ToInt32(Console.ReadLine());
-
-                if (number < 0)
-                {
-                    continue;
-                }
-                else if (number == 0)
-                {
-                    break;
-                }
-
-                // number > 0
-                sum += number;
+                Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
+                favcolors[i] = Console.ReadLine();
             }
 
-            Console.WriteLine("Итоговая сумма: {0}", sum);
-        
-            Console.ReadKey();
-            
+            foreach (var color in favcolors)
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is yellow!");
+                        break;
+                }
+
+
+                Console.ReadKey();
+            }
         }
     }
 }
