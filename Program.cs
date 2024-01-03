27a99b6ffff4ff40639adbb96f0cc2a7b88909bc
татьяна090975 <tatyana090975@gmail.com>
2024-01-04 03:10:55 +0400

    Задание 4.3.13

--- Program.cs
+++ Program.cs
@@ -12,25 +12,14 @@ namespace Mod4BasicOfC
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
 
-            int temp;
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
-            }
+            int sum = 0;
 
             foreach (var item in arr)
             {
-                Console.Write(item);
+                sum = sum + item;               
             }
+
+            Console.WriteLine(sum);
             Console.ReadKey();
          
         }
