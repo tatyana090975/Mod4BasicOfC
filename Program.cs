0dc34410cb60b672938a9bf211d60438748c8a7b
татьяна090975 <tatyana090975@gmail.com>
2024-01-04 03:46:21 +0400

    Задание 4.3.16

--- Program.cs
+++ Program.cs
@@ -11,16 +11,20 @@ namespace Mod4BasicOfC
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] {1, 2, 3, 4, 5, 6, -5, -6 };
+            int[,] array = new int[,] { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
 
             int summ = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.GetUpperBound(0)+1; i++)
             {
-                if (array[i] > 0)
+                for (int j = 0; j < array.GetUpperBound(1)+1; j++)
                 {
-                    summ += 1;
+                    if (array[i, j] > 0)
+                    {
+                        summ += 1;
+                    }
                 }
+                
             }
 
                      
