ad77ae8c7f5f1dd9473d19ae6585806729fadd29
татьяна090975 <tatyana090975@gmail.com>
2024-01-03 23:05:05 +0400

    Задание 4.1.16

--- Program.cs
+++ Program.cs
@@ -11,12 +11,32 @@ namespace Mod4BasicOfC
         static void Main(string[] args)
         {
             {
-                var a = 6;
-                var b = 7;
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-                var c = a != b ? a + b : b;
+                var color = Console.ReadLine();
 
-                Console.WriteLine(c);
+                if (color == "red")
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                }
+
+                else if (color == "green")
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                }
                 Console.ReadKey();
             }
         }
