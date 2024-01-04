89e2c927354ced03de6c396126c44708aa472a3e
татьяна090975 <tatyana090975@gmail.com>
2024-01-04 12:58:17 +0400

    Задание 4.5.6

--- Program.cs
+++ Program.cs
@@ -15,38 +15,42 @@ namespace Mod4BasicOfC
         {
             (string name, string surname, string login, int loginCount, bool pet, double age, string[] favcolors) anketa;
             string pet;
-            Console.WriteLine("Введите имя: ");
-            anketa.name = Console.ReadLine();
-            Console.WriteLine("Введите фамилию: ");
-            anketa.surname = Console.ReadLine();
-            Console.WriteLine("Введите логин: ");
-            anketa.login = Console.ReadLine();
-            anketa.loginCount = anketa.login.Length;
-            Console.WriteLine("Есть ли у вас животные? Да или Нет: ");
-            pet = Console.ReadLine();
-            if (pet == "Да")
+            for (int j = 0; j < 3; j++)
             {
-                anketa.pet = true;
+                Console.WriteLine("Введите имя: ");
+                anketa.name = Console.ReadLine();
+                Console.WriteLine("Введите фамилию: ");
+                anketa.surname = Console.ReadLine();
+                Console.WriteLine("Введите логин: ");
+                anketa.login = Console.ReadLine();
+                anketa.loginCount = anketa.login.Length;
+                Console.WriteLine("Есть ли у вас животные? Да или Нет: ");
+                pet = Console.ReadLine();
+                if (pet == "Да")
+                {
+                    anketa.pet = true;
+                }
+                else
+                {
+                    anketa.pet = false;
+                }
+                Console.WriteLine("Введите возраст: ");
+                anketa.age = Convert.ToDouble(Console.ReadLine());
+                anketa.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета: ");
+                for (int i = 0; i < 3; i++)
+                {
+                    anketa.favcolors[i] = Console.ReadLine();
+                }
+                /*
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(anketa.favcolors[i]);
+                }*/
             }
-            else
-            {
-                anketa.pet = false;
-            }
-            Console.WriteLine("Введите возраст: ");
-            anketa.age = Convert.ToDouble(Console.ReadLine());
-            anketa.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета: ");
-            for (int i = 0; i < 3; i++)
-            {
-                anketa.favcolors[i] = Console.ReadLine();
-            }
-            /*
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine(anketa.favcolors[i]);
-            }*/
-            
-           
+
+
+
 
 
             Console.ReadKey();
